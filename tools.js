4c0b9b1bd57d92b3218e705385745d42e65e9d21
Bobby Gumpus <rlawry@southlewis.org>
2023-01-17 16:53:34 -0500

    southern hemisphere works

--- tools.js
+++ tools.js
@@ -1,6 +1,6 @@
 const c = document.getElementById("canvas");
 const ctx = c.getContext("2d");
-let lat = 50;
+let lat = -10;
 var fudge = 0;
 
 var latitudes = [
@@ -76,126 +76,6 @@ function drawGround(){
     ctx.stroke();
     ctx.fill();
     ctx.closePath();
-}
-
-function establishClip(){
-    //ctx.save();
-    ctx.beginPath();
-    ctx.translate(c.width/2,c.height/2);
-    ctx.arc(0,0,200,0,Math.PI,true);
-    ctx.ellipse(0,0,200,40,0,Math.PI,0,true);
-    ctx.fillStyle="white";
-    ctx.closePath();
-    //ctx.fill();
-    //ctx.clip();
-}
-
-function rad(deg){
-    return deg*Math.PI/180;
-}
-
-function drawEquinoxPath(lat){
-    ctx.save();
-    //ctx.translate(c.width/2,c.height/2);
-    let phi = Math.atan(40*(Math.cos(rad(90-lat))/200*Math.tan(rad(90-lat))));
-    ctx.beginPath();
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = colors[1];
-    ctx.rotate(rad(90-lat));
-    ctx.ellipse(0,0,200,40*Math.cos(rad(90-lat)),0,Math.PI/2-phi,3*Math.PI/2-phi,false);
-    ctx.stroke();
-    ctx.closePath();
-    ctx.restore();
-}
-
-function drawSummerSolstice(lat){
-    let angle=90-lat;
-    let offset = 79.749813785;  //tilt of 23.5° gets us this value.
-    let newCenter = {
-        x:offset*Math.sin(rad(angle)),
-        y:-offset*Math.cos(rad(angle))
-    };
-    
-    let newDiameter = 200*Math.sin(Math.acos(offset/200));
-    let scale40 = newDiameter/200;
-    let new40 = scale40*40;
-
-    let phi = Math.atan(40*(Math.cos(rad(90-lat))/200*Math.tan(rad(90-lat))));
-    let newPhi = Math.atan(new40/newDiameter*Math.tan(Math.acos(offset*Math.tan(rad(lat)/newDiameter))));
-    let ex = offset*Math.tan(rad(lat));
-    console.log(ex + " x value");
-    let circlePhi = Math.acos(ex/newDiameter);
-    console.log(deg(circlePhi) + " deg(circlePhi)");
-    // let why = new40*Math.sin(circlePhi);
-    // console.log(why + " y value");
-    if(ex >= newDiameter){
-        circlePhi = 0;
-        phi = 0;
-    }
-    let startAngle = deg(circlePhi-phi)-fudge;
-    let endAngle = deg(2*Math.PI-circlePhi-phi)-fudge;
-    console.log(startAngle + " startAngle and " + endAngle + " endAngle");
-    // let testAngle = Math.atan(why/ex);
-    // console.log(testAngle + " rad test angle and " + deg(testAngle) + "deg test angle");
-    ctx.save();
-    //ctx.translate(c.width/2,c.height/2);
-    ctx.translate(newCenter.x,newCenter.y);
-    ctx.beginPath();
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = colors[2];
-    ctx.rotate(rad(90-lat));
-    ctx.ellipse(0,0,newDiameter,40*Math.cos(rad(angle))*scale40,0,rad(startAngle),rad(endAngle),false);
-    
-    ctx.stroke();
-    ctx.closePath();
-    ctx.restore();
-}
-
-function deg(theta){
-    return theta*180/Math.PI;
-}
-
-function drawWinterSolstice(lat){
-    let angle=90-lat;
-    let offset = 79.749813785;
-    let newCenter = {
-        x:-offset*Math.sin(rad(angle)),
-        y:offset*Math.cos(rad(angle))
-    };
-    let newDiameter = 200*Math.sin(Math.acos(offset/200));
-    let scale40 = newDiameter/200;
-
-    //let new40 = scale40*40;
-
-    let phi = Math.atan(40*(Math.cos(rad(90-lat))/200*Math.tan(rad(90-lat))));
-    //let newPhi = Math.atan(new40/newDiameter*Math.tan(Math.acos(offset*Math.tan(rad(lat)/newDiameter))));
-    let ex = -1*offset*Math.tan(rad(lat));
-    console.log(ex + " x value");
-    let circlePhi = Math.acos(ex/newDiameter);
-    console.log(deg(circlePhi) + " deg(circlePhi)");
-    // let why = new40*Math.sin(circlePhi);
-    // console.log(why + " y value");
-    if(ex >= newDiameter){
-        circlePhi = 0;
-        phi = 0;
-    }
-    let startAngle = deg(circlePhi-phi);
-    let endAngle = deg(2*Math.PI-circlePhi-phi);
-    console.log(startAngle + " startAngle and " + endAngle + " endAngle");
-    // let testAngle = Math.atan(why/ex);
-    // console.log(testAngle + " rad test angle and " + deg(testAngle) + "deg test angle");
-    ctx.save();
-    //ctx.translate(c.width/2,c.height/2);
-    ctx.translate(newCenter.x,newCenter.y);
-    ctx.beginPath();
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = colors[0];
-    ctx.rotate(rad(90-lat));
-    ctx.ellipse(0,0,newDiameter,40*Math.cos(rad(angle))*scale40,0,rad(startAngle),rad(endAngle),false);
-    
-    ctx.stroke();
-    ctx.closePath();
-    ctx.restore();
 }
 
 function drawSky(){
@@ -294,6 +174,161 @@ function drawSmallIncrements(){
     ctx.restore();
 }
 
+function establishClip(){
+    //ctx.save();
+    ctx.beginPath();
+    ctx.translate(c.width/2,c.height/2);
+    ctx.arc(0,0,200,0,Math.PI,true);
+    ctx.ellipse(0,0,200,40,0,Math.PI,0,true);
+    ctx.fillStyle="white";
+    ctx.closePath();
+    //ctx.fill();
+    //ctx.clip();
+}
+
+function rad(deg){
+    return deg*Math.PI/180;
+}
+
+let startAngle, endAngle;
+
+function drawEquinoxPath(lat){
+    ctx.save();
+    //ctx.translate(c.width/2,c.height/2);
+    let phi = Math.atan(40*(Math.cos(rad(90-lat))/200*Math.tan(rad(90-lat))));
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = colors[1];
+    if(lat>=0){
+        startAngle = Math.PI/2-phi-fudge;
+        endAngle = 3*Math.PI/2-phi-fudge;
+    }
+    else if(lat<0){
+        startAngle = Math.PI/2+phi-fudge;
+        endAngle = 3*Math.PI/2+phi-fudge;
+    }
+    ctx.rotate(rad(90-lat));
+    ctx.ellipse(0,0,200,Math.abs(40*Math.cos(rad(90-lat))),0,startAngle,endAngle,false);
+    ctx.stroke();
+    ctx.closePath();
+    ctx.restore();
+}
+
+function cos(theta){
+    return Math.cos(Math.abs(theta));
+}
+
+function sin(theta){
+    return Math.sin(Math.abs(theta));
+}
+
+function tan(theta){
+    return Math.tan(theta);
+}
+
+function drawSummerSolstice(lat){
+    let angle=90-lat;
+    let offset = 79.749813785;  //tilt of 23.5° gets us this value.
+    let newCenter = {
+        x:offset*Math.sin(rad(angle)),
+        y:-offset*Math.cos(rad(angle))
+    };
+    
+    let newDiameter = 200*Math.sin(Math.acos(offset/200));
+    let scale40 = newDiameter/200;
+    let new40 = scale40*40;
+
+    let phi = Math.atan(40*(Math.cos(rad(90-lat))/200*Math.tan(rad(90-lat))));
+    let newPhi = Math.atan(new40/newDiameter*Math.tan(Math.acos(offset*Math.tan(rad(lat)/newDiameter))));
+    let ex = offset*Math.tan(rad(lat));
+    console.log(ex + " x value");
+    let circlePhi = Math.acos(ex/newDiameter);
+    console.log(deg(circlePhi) + " deg(circlePhi)");
+    // let why = new40*Math.sin(circlePhi);
+    // console.log(why + " y value");
+    if(ex >= newDiameter){
+        circlePhi = 0;
+        phi = 0;
+    }
+    if(lat>=0){
+        startAngle = deg(circlePhi-phi)-fudge;
+        endAngle = deg(2*Math.PI-circlePhi-phi)-fudge;
+    }
+    else if(lat<0){
+        startAngle = deg(circlePhi+phi)-fudge;
+        endAngle = deg(2*Math.PI-circlePhi+phi)-fudge;
+    }
+    console.log(startAngle + " startAngle and " + endAngle + " endAngle");
+    // let testAngle = Math.atan(why/ex);
+    // console.log(testAngle + " rad test angle and " + deg(testAngle) + "deg test angle");
+    ctx.save();
+    //ctx.translate(c.width/2,c.height/2);
+    ctx.translate(newCenter.x,newCenter.y);
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = colors[2];
+    ctx.rotate(rad(90-lat));
+    ctx.ellipse(0,0,newDiameter,Math.abs(40*Math.cos(rad(angle)))*scale40,0,rad(startAngle),rad(endAngle),false);
+    
+    ctx.stroke();
+    ctx.closePath();
+    ctx.restore();
+}
+
+function deg(theta){
+    return theta*180/Math.PI;
+}
+
+function drawWinterSolstice(lat){
+    let angle=90-lat;
+    let offset = 79.749813785;
+    let newCenter = {
+        x:-offset*Math.sin(rad(angle)),
+        y:offset*Math.cos(rad(angle))
+    };
+    let newDiameter = 200*Math.sin(Math.acos(offset/200));
+    let scale40 = newDiameter/200;
+
+    //let new40 = scale40*40;
+
+    let phi = Math.atan(40*(Math.cos(rad(90-lat))/200*Math.tan(rad(90-lat))));
+    //let newPhi = Math.atan(new40/newDiameter*Math.tan(Math.acos(offset*Math.tan(rad(lat)/newDiameter))));
+    let ex = -1*offset*Math.tan(rad(lat));
+    console.log(ex + " x value");
+    let circlePhi = Math.acos(ex/newDiameter);
+    console.log(deg(circlePhi) + " deg(circlePhi)");
+    // let why = new40*Math.sin(circlePhi);
+    // console.log(why + " y value");
+    if(ex >= newDiameter){
+        circlePhi = 0;
+        phi = 0;
+    }
+    if(lat>=0){
+        startAngle = deg(circlePhi-phi)-fudge;
+        endAngle = deg(2*Math.PI-circlePhi-phi)-fudge;
+    }
+    else if(lat<0){
+        startAngle = deg(circlePhi+phi)-fudge;
+        endAngle = deg(2*Math.PI-circlePhi+phi)-fudge;
+    }
+    console.log(startAngle + " startAngle and " + endAngle + " endAngle");
+    // let testAngle = Math.atan(why/ex);
+    // console.log(testAngle + " rad test angle and " + deg(testAngle) + "deg test angle");
+    ctx.save();
+    //ctx.translate(c.width/2,c.height/2);
+    ctx.translate(newCenter.x,newCenter.y);
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = colors[0];
+    ctx.rotate(rad(90-lat));
+    ctx.ellipse(0,0,newDiameter,Math.abs(40*Math.cos(rad(angle)))*scale40,0,rad(startAngle),rad(endAngle),false);
+    
+    ctx.stroke();
+    ctx.closePath();
+    ctx.restore();
+}
+
+
 window.onresize = function(){
     init();
 }
@@ -309,8 +344,8 @@ function animate(){
         else {lat=90; up=false;}
     }
     else if(!up){
-        if(lat>0){lat-=1;}
-        else{lat = 0; up=true;}
+        if(lat>-90){lat-=1;}
+        else{lat = -90; up=true;}
     }
     init();
     requestAnimationFrame(animate); 
